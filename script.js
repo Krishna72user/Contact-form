987a5613fac150bff0eac07a7870a40d61d31daa
Krishna <anonymous@google.com>
2024-12-05 18:22:46 +0530

    Improved UI

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwMLFSlV_916O8uLhXFZWeJTvY_Lq3yEK2nPtUyjSjOYqfqmnsz--zizioFFe2oskl4/exec'
 
 const form = document.forms['form']
-
 form.addEventListener('submit', e => {
   e.preventDefault();
-  document.getElementById("submit").innerText="Sending";
+  const newf = document.createElement("div")
+  newf.className="New";
+  newf.innerText="Sending..."
+  document.getElementById("submit").replaceWith(newf);
   fetch(scriptURL, { method: 'POST',
     body: new FormData(form)})
   .then(response => alert("Thank you! Form is submitted" ))
